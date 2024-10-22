06358857113b9537fdbe08d868085fd86f90b799
Estiven Alvarez <Estiven.Alvarez@TRANESMEDELLIN.LOCAL>
2024-10-21 20:35:14 -0500

    SpringBoot Ready!

---
+++ App/src/main/java/app/dao/interfaces/PersonDao.java
@@ -0,0 +1,16 @@
+package app.dao.interfaces;
+
+import app.dto.PersonDto;
+
+
+public interface PersonDao {
+
+    public boolean existByDocument(PersonDto personDto) throws Exception;
+
+    public void createPerson(PersonDto personDto) throws Exception;
+
+    public void deletePerson(PersonDto personDto) throws Exception;
+
+    public PersonDto findByDocument(PersonDto personDto) throws Exception;
+
+}
